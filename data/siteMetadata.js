394708c52e977622085625eba60d68cacefa99d7
Joshua Voydik <voydik@gmail.com>
2022-05-18 11:18:11 -0700

    linkedin

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -15,7 +15,7 @@ const siteMetadata = {
   twitter: 'https://twitter.com/joshuavoydik',
   facebook: 'https://facebook.com',
   youtube: 'https://youtube.com',
-  linkedin: 'https://www.linkedin.com',
+  linkedin: 'https://www.linkedin.com/in/joshuavoydik',
   locale: 'en-US',
   analytics: {
     // If you want to use an analytics provider you have to add it to the
