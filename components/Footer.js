9f16a8c07cf5bbb38813b559705ccd430e154b38
Joshua Voydik <voydik@gmail.com>
2022-05-18 11:20:23 -0700

    removed dot from footer

--- components/Footer.js
+++ components/Footer.js
@@ -15,7 +15,6 @@ export default function Footer() {
         <div className="mb-2 flex space-x-2 text-sm text-gray-500 dark:text-gray-400">
           <div>{siteMetadata.author}</div>
           <div>{`© ${new Date().getFullYear()}`}</div>
-          <div>{` • `}</div>
           <Link href="/">{siteMetadata.title}</Link>
         </div>
       </div>
