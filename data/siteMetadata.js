bf9abd92d0dc39e5c646e3f6626281a2f72a14c7
Joshua <voydik@gmail.com>
2022-05-23 13:22:09 -0700

    asdfasd

--- data/siteMetadata.js
+++ data/siteMetadata.js
@@ -7,7 +7,7 @@ const siteMetadata = {
   theme: 'system', // system, dark or light
   siteUrl: 'https://joshuavoydik.com',
   siteRepo: 'https://github.com/joshuavoydik/website',
-  siteLogo: '',
+  siteLogo: '/static/images/logo.png',
   image: '/static/images/avatar.png',
   socialBanner: '/static/images/twitter-card.png',
   email: 'voydik@gmail.com',
